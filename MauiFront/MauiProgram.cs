79d3c8ada6a9c3230176031f22779efc73fd1edb
JorgeCayambe <andrescayambe21@gmail.com>
2026-04-07 12:11:20 +0200

    Limpiando carpetas ignoradas

--- MauiFront/MauiProgram.cs
+++ MauiFront/MauiProgram.cs
@@ -1,14 +1,20 @@
 ﻿using Microsoft.Extensions.Logging;
 
+using Syncfusion.Maui.Core.Hosting;
+
 namespace MauiFront
 {
     public static class MauiProgram
     {
         public static MauiApp CreateMauiApp()
         {
+            
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JHaF5cWWdCekx3QXxbf1x2ZFZMZFtbQXZPMyBoS35RcEVnWXhec3BSQ2BcWER2VEFe");
+
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .ConfigureSyncfusionCore() 
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -16,7 +22,7 @@ namespace MauiFront
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
