bee09f3930933c3f8ae04cc39d30c323d3840c88
JORGEGOOD <andrescayambe21@gmail.com>
2026-04-22 11:51:04 +0200

    commit

--- NetUtils/Class1.cs
+++ NetUtils/Class1.cs
@@ -31,7 +31,7 @@ namespace NetUtils
 
         public static Socket ConnectToServer()
         {
-            Socket socket = CreateClientSocket("192.168.111.39", 8888);
+            Socket socket = CreateClientSocket("192.168.111.40", 8888);
             return socket;
         }
 
